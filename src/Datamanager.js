0a618d4f10a7d0d2bdba8ff68cbb45b04c4d92c8
Shubham Aggarwal <viv.shubham077@gmail.com>
2020-10-01 14:06:57 +0530

    added function for table search

--- src/Datamanager.js
+++ src/Datamanager.js
@@ -1,10 +1,9 @@
-const electron = require('electron');
-const { ipcMain } = electron;
 const fs = require('fs');
-const randomWords = require('random-words');
 const path = require('path');
+const lineReader = require('line-reader');
+let Promise = require("bluebird");
 
-
+let searchedData = ["NOTFOUND"];
 
 
 const createData = async (totalRows) => {
@@ -12,7 +11,11 @@ const createData = async (totalRows) => {
    const dataDir = path.join(dir,'data');
    createDirectory(dir);
    createDirectory(dataDir);
-
+    
+   let metadataFilePath = path.join(dataDir,"metadata");
+   let metadata = totalRows;
+   writeRow(metadataFilePath, metadata);
+   
    let extentNumber = 1;
    let pageNumber = 1;
    let pageLength = 0;
@@ -22,9 +25,9 @@ const createData = async (totalRows) => {
         createDirectory(extentPath);
 
         const rollNo = i;
-        const name = await getRandomWord();
-        const username = await getRandomWord();
-        const password = await  getRandomWord();
+        const name = getRandomWord();
+        const username = getRandomWord();
+        const password =  getRandomWord();
         const rowData = `${rollNo}|${name}|${username}|${password}\n`;
         pageLength += rowData.length;
 
@@ -68,4 +71,58 @@ const writeRow = (file,rowData) => {
     }
 }
 
-module.exports = createData;
+async function searchData(dataToBeDisplayed,searchDataInColumn,dataToBeSearched){
+    
+   const dir = path.join(__dirname,'GeneratedData');
+   const dataDir = path.join(dir,'data');
+    let extentNumber=1,pageNumber=1;
+    let totalRows=0;
+    let metadataFilePath = path.join(dataDir,"metadata");
+    let curLine = "";
+    let resultAfterSearch;
+
+    var eachLine = Promise.promisify(lineReader.eachLine);
+    await eachLine(metadataFilePath, function(line) {
+        curLine = line.toString();
+    }).then(async function() {
+        totalRows = parseInt(curLine);
+        // console.log(totalRows);
+        let i=1;
+        let flag = false;
+        while (i <= parseInt(totalRows)) {
+            if(flag)break;
+            let extentPath = path.join(dataDir,"extent_"+extentNumber);
+            let pagePath = path.join(extentPath,"page_"+pageNumber+".txt");
+            // let pathsearch = path.relative(__filename,pagePath);
+
+            let pathsearch = pagePath;
+            pathsearch = pathsearch.split("\\").join("/");
+            console.log(pathsearch);
+            await eachLine(pathsearch,function(line){
+                if(flag == false){
+                    // console.log(line);
+                curLine = line.toString();
+                let result = curLine.split("|");
+                i++;
+                if(dataToBeSearched==result[parseInt(searchDataInColumn)]){
+                    resultAfterSearch = result;
+                    flag = true;
+                } else return;
+            }
+            }).then(()=>{
+                pageNumber++;
+                if (pageNumber == 9) {
+                    extentNumber++;
+                    pageNumber = 1;
+                }
+            })
+        }
+    }).catch(function(err) {
+        console.error(err);
+    });
+    if(resultAfterSearch)searchedData = resultAfterSearch;
+    else searchedData = ["NOTFOUND"];
+    
+}
+
+export {createData,searchData,searchedData};
